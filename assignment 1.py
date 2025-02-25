c225d3e0bb953c2859c9a7743f1bab13110c0e8b
RyanK399 <1kerrryA2@hdsb.ca>
2025-02-25 14:21:04 -0500

    new modifications

--- assignment 1.py
+++ assignment 1.py
@@ -1,4 +1,28 @@
 #Name Ryan Kerr
-#Date modified:Febuary 24,2025
+#Date modified:Febuary 25,2025
 
+# integer is any whole number positive or negative up to 2 million
+x = 0
+#  Float is a number with a decimal point
+y = 0.0
+# Boolean is true or false
+z = true
+# char is single character
+# string is combination of charicters
+# double is more acurate number with decemal points
+print(x)
+print(y)
+print(z)
 
+ print(2*3)
+ # this * multiplies
+ print(5/2)
+ #this / divides
+ print(2+3)
+ #this + does addition
+  print(7%4)
+  #this gives remainders
+  (2**3)
+  #this does exponents
+(math.sqrt(9))
+  # this does square roots
